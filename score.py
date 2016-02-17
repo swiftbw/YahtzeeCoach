09f0c9d0a66884b0848091a81e9cccfd12bdd2f9
Brian Swift <swiftbw@gmail.com>
2016-02-17 12:35:40 -0800

    Added Score class and YahtzeeCoach main

---
+++ score.py
@@ -0,0 +1,14 @@
+
+
+
+class Score():
+    score = [ "1s", "2s", "3s", "4s", "5s", "6s", "3k", "4k", "Bt", "Sm", "Lg", "Yz", "Ch" ]
+    def odds():
+        print "woohoo"
+
+
+def main():
+    print "Hello"
+
+if __name__ == '__main__':
+    main()
